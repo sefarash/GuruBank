10a11fabd2dbe97592338babcd19cd4d4c025c62
Rashad Ismayilov <ismayilov.rashad@gmail.com>
2018-07-24 14:37:53 -0500

    add Guruproject

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import util.Driver;
+
+public class LoginPage {
+	public LoginPage() {
+		PageFactory.initElements(Driver.getDriver(), this);	
+	}
+	
+	@FindBy(name="uid")
+	public WebElement userID;
+	
+	@FindBy(name="password")
+	public WebElement password;
+	
+	
+	
+	}
+
+
